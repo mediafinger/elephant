fbaddb881dcffd4f54cf7f70bf70c132baa2b14e
Andreas Finger <andy@mediafinger.com>
2014-08-14 16:29:43 +0200

    Let the user enter his tax rate

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -6,6 +6,7 @@ class CalculatorController < ApplicationController
   def create
     @items          = params[:items].to_i
     @price_per_item = params[:price_per_item].to_i
+    @tax            = params[:tax].to_i
 
     @total_amount = calculate_price
   end
@@ -13,7 +14,7 @@ class CalculatorController < ApplicationController
   private
 
   def calculate_price
-    (@items * @price_per_item) * 1.21
+    (@items * @price_per_item) * (1 + @tax.to_f/100)
   end
 
 end
