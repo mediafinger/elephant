2441a8e42fd4a9bffd9c3c3b0c2550baa94ea5fd
Andreas Finger <andy@mediafinger.com>
2014-08-14 16:25:33 +0200

    Add tax for ES

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -7,7 +7,13 @@ class CalculatorController < ApplicationController
     @items          = params[:items].to_i
     @price_per_item = params[:price_per_item].to_i
 
-    @total_amount = @items * @price_per_item
+    @total_amount = calculate_price
+  end
+
+  private
+
+  def calculate_price
+    (@items * @price_per_item) * 1.21
   end
 
 end
